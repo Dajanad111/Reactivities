82c26e59438d05a9c56de06260b8fc7ddc5520db
Dajanad111 <dajana.dragnic@softnex.me>
2026-02-03 14:29:09 +0100

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -7,22 +9,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
-    
-opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
 
 builder.Services.AddCors();
+builder.Services.AddMediatR(x =>
+x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000","https://localhost:3000"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
-var services= scope.ServiceProvider;
-try 
+var services = scope.ServiceProvider;
+try
 {
     var context = services.GetRequiredService<AppDbContext>();
     await context.Database.MigrateAsync();
